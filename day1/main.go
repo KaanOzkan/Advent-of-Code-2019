8bd2ea6ed1f6dc54206483989d55068db037b087
Kaan Ozkan <kaanozkan97@gmail.com>
2019-12-01 18:02:19 -0800

    part2 done

--- day1/main.go
+++ day1/main.go
@@ -3,53 +3,49 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"os"
 	"strconv"
 )
 
-var TotalFuel = 0
-
-func calc(num string) int {
-	i, err := strconv.Atoi(num)
-	if err != nil {
-		return 0
-		log.Fatal(err)
-	}
-	i = i / 3
-	return i - 2
+func calc(num int) int {
+	num = num / 3
+	return num - 2
 }
 
 func main() {
 	file, err := os.Open("input.txt")
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 	defer file.Close()
 
+	var Total = 0
+	var Total2 = 0
+
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		TotalFuel += calc(scanner.Text())
+		num, err := strconv.Atoi(scanner.Text())
+
+		if err != nil {
+			break
+		}
+
+		Total += calc(num)
+		fuelLeft := num
+
+		for {
+			fuelLeft = calc(fuelLeft) // convert to string to use same method
+			if fuelLeft < 0 {
+				break
+			}
+			Total2 += fuelLeft
+		}
 	}
 
-	fmt.Println(TotalFuel)
-
-	// TotalFuel = 100756 // TODO: Remove
-    // original := TotalFuel
-
-	// fuelLeft := TotalFuel
-	// for fuelLeft > 0 {
-	// 	fuelLeft = calc(strconv.Itoa(fuelLeft)) // convert to string to use same method
-	// 	// fmt.Println(fuelLeft)
-	// 	// fmt.Println(TotalFuel)
-	// 	if fuelLeft > 0 {
-	// 		TotalFuel += fuelLeft
-	// 	}
-	// }
-
-	// fmt.Println(TotalFuel) // Result for part 2
+	fmt.Printf("Part 1: %d\n", Total) // Result of Part 1
+	fmt.Printf("Part 2: %d", Total2)  // Result of Part2
 
 	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 }
